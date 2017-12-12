5b2621e5755942d00c244d16581dfd20b1032cca
larsonma <larsonm2222@gmail.com>
2017-12-12 17:05:42 -0600

    Added user Object to models

---
+++ models/user.js
@@ -0,0 +1,15 @@
+class User {
+
+    constructor(userData) {
+      this.name = userData.name;
+      this.displayName = userData.displayName
+      this.email = userData.email
+      this.emailVerified = userData.emailVerified
+      this.photoURL = userData.photoURL
+      this.isAnonymous = userData.isAnonymous
+      this.uid = userData.uid
+      this.providerData = userData.providerData
+      this.fcmToken = userData.fcmToken
+    }
+        
+  }
