5c42ed71bbc3363b74719da91dbe8c0b519bc105
larsonma <larsonm2222@gmail.com>
2017-12-12 17:29:51 -0600

    Adding typescript user file

---
+++ models/user.ts
@@ -0,0 +1,23 @@
+class User {
+
+  private displayName;
+  private email;
+  private emailVerified;
+  private photoURL;
+  private isAnonymous;
+  private uid;
+  private providerData;
+  private fcmToken;
+
+    constructor(userData) {
+      this.displayName = userData.displayName
+      this.email = userData.email
+      this.emailVerified = userData.emailVerified
+      this.photoURL = userData.photoURL
+      this.isAnonymous = userData.isAnonymous
+      this.uid = userData.uid
+      this.providerData = userData.providerData
+      this.fcmToken = userData.fcmToken
+    }
+        
+  }module.exports = User;
