f8c118fc273402f4f7a8c1eab3a7811212434234
Matt <matthew.watts.mw@gmail.com>
2017-12-22 21:49:31 -0600

    testing heroku deploy

--- index.js
+++ index.js
@@ -16,16 +16,20 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
+
 app.post('/initshipment', (req, res) => {
   let shipment = req.body;
   let users = db.collection('UserData');
   users.where('phoneNumber', '==', req.body.recipient).get().then(result => {
     result.forEach(user => {
-      let notification = {
+      let data = {
         message: `You have a new shipment pending from ${shipment.sender}.`
       }
       users.doc(user.id).collection('pendingShipments').add(shipment).then(() => {
-        admin.messaging().sendToDevice(user.get('fcmToken'), { 'data': notification }).catch(err => console.error(err))
+        admin.messaging().sendToDevice(user.get('fcmToken'), { data }).catch(err => console.error(err))
         res.sendStatus(200)
       }).catch(err => {
         console.error(err)
